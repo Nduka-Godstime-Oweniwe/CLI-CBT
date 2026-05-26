77fa97c7df637e8f648d51851b85ce59ee32b10a
ayomide_shogbesan <ayomideshogbesan@gmail.com>
2026-05-26 20:22:27 +0100

    User question input is a goal

--- functions/getquestion.go
+++ functions/getquestion.go
@@ -1,14 +1,92 @@
-package cbt
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func UserInput(prompt string) string {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print(prompt)
+
+	input, _ := reader.ReadString('\n')
+
+	// Remove newline character
+	input = strings.TrimSpace(input)
+
+	return input
+}
 
 func UserQuestion() (string, string) {
-	// This function Uses UserInput("Enter QuestionL ")
-	//UserInput("Enter Option A: ")
-	// UserInput("Enter Option B: ")
-	// UserInput("Enter Answer : ")
-	// Questions and options must not be empty
-	// Answers must be A or B. If user types in a or b convert to upper.
-	// Joint Question, OptionA and OptionB together as one string but separated by a newline character "\n"
-	//Return both values
-	return "question", "answer"
+	var question string
+	var optionA string
+	var optionB string
+	var answer string
+	// This function Uses UserInput("Enter Question ")
+	for {
 
+		for {
+			question = UserInput("Enter Question: ")
+			if question == "" {
+				fmt.Print("Question cannot be empty\n")
+				continue
+			}
+			break
+		}
+		//UserInput("Enter Option A: ")
+		for {
+			optionA = UserInput("Enter Option A: ")
+			if optionA == "" {
+				fmt.Print("Options cannot be empty\n")
+				continue
+			}
+			break
+		}
+		// UserInput("Enter Option B: ")
+		for {
+			optionB = UserInput("Enter Option B: ")
+			if optionB == "" {
+				fmt.Print("Options cannot be empty\n")
+				continue
+			}
+			break
+		}
+		// UserInput("Enter Answer : ")
+		for {
+			answer = UserInput("Enter Answer (A or B): ")
+			if answer == "" {
+				fmt.Print("Answer cannot be empty\n")
+				continue
+			}
+			break
+		}
+
+		// Answers must be A or B. If user types in a or b convert to upper.
+		for {
+			answer = strings.ToUpper(answer)
+			if answer != "A" && answer != "B" {
+				fmt.Print("Answer must be A or B\n")
+				continue
+			}
+			break
+		}
+		break
+	}
+
+	// Joint Question, OptionA and OptionB together as one string but separated by a newline character "\n"
+	fullQuestion := question +
+		"\nA. " + optionA +
+		"\nB. " + optionB
+
+	//Return both values
+	return fullQuestion, answer
+
+}
+
+func main() {
+	UserQuestion()
 }
